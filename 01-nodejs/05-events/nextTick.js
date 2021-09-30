e70ef913b2780a8a3eb35782f380a33e8c4e6a9f
india jang <jangindia1@gmail.com>
2021-10-01 01:47:33 +0900

    [211001] Study: events #1

---
+++ 01-nodejs/05-events/nextTick.js
@@ -0,0 +1,15 @@
+const { EventEmitter, once } = require('events');
+
+const myEE = new EventEmitter();
+
+async function foo() {
+  await Promise.all([once(myEE, 'bar'), once(myEE, 'foo')]);
+  console.log('foo', 'bar');
+}
+
+process.nextTick(() => {
+  myEE.emit('bar');
+  myEE.emit('foo');
+});
+
+foo().then(() => console.log('done'));
--- 02-shell_script/redirect_pipe.sh
+++ 02-shell_script/redirect_pipe.sh
@@ -3,7 +3,7 @@
 ### Concept ###
 # Redirect : 프로그램의 결과나 출력을 파일이나 스트림으로 전달함
 # Pipe     : 프로세스 혹은 실행된 프로그램의 결과를 프로그램으로 넘겨줌
-# Redirect, Pipe는 IPC(Interprocess Communication) 에 속함
+# Redirect, Pipe는 IPC(Interprocess Communication)에 속함
 
 ### Linux Command ###
 # tee : 표준 입력(standard input)에서 읽어서 표준 출력(standard output)과 파일에 쓰는 명령어
