19d0b147d1db653d2bc8011fbc1f5d57d8260380
india jang <jangindia1@gmail.com>
2021-11-01 23:37:55 +0900

    [211101] mongodb install

---
+++ 01-nodejs/08-frameworks/express_example/login/controllers/AuthController.js
@@ -0,0 +1,24 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User"); // User model
+
+const { registerSchema, loginSchema } = require('../utils/userValidations');
+
+exports.registerUser = (req, res) => {
+
+};
+
+exports.unregisterUser = (req, res) => {
+
+};
+
+exports.loginUser = (req, res) => {
+    
+};
+
+exports.logoutUser = (req, res) => {
+
+};
+
+exports.authChecker = (req, res) => {
+
+};
--- 01-nodejs/08-frameworks/express_example/login/routes/users.js
+++ 01-nodejs/08-frameworks/express_example/login/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const { registerUser, loginUser, logoutUser, authChecker } = require("../controllers/AuthController");
+const { registerLimiter, loginLimiter } = require("../utils/rateLimiter");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get("/authchecker", authChecker );
+
+router.post("/register", registerLimiter, registerUser );
+
+router.post("/login", loginLimiter, loginUser );
+
+router.delete("/logout", logoutUser );
 
 module.exports = router;
