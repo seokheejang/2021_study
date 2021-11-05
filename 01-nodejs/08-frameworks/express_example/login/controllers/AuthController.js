c1d523c5f36ff6f57ea9e7e302c010e81eba49b5
india jang <jangindia1@gmail.com>
2021-11-05 23:58:46 +0900

    [211105] loginUser

--- 01-nodejs/08-frameworks/express_example/login/controllers/AuthController.js
+++ 01-nodejs/08-frameworks/express_example/login/controllers/AuthController.js
@@ -56,20 +56,22 @@ exports.loginUser = (req, res) => {
     const { email, password } = req.body;
 
     // basic validation
-    const result = loginSchema.validate({ email, password});
+    //const result = loginSchema.validate({ email, password});
+    const result = { error: false };
     if (!result.error) {
         //check for existing user
         User.findOne({ email }).then((user) => {
             if (!user) return res.status(400).json("Incorrect Email or Password");
-
+            console.log("find user", user);
             // Validate password
             bcrypt.compare(password, user.password).then((isMatch) => {
                 if (!isMatch) return res.status(400).json("Incorrect Email or Password");
 
                 const sessUser = { id: user.id, name: user.name, email: user.email };
-                req.session.user = sessUser; // Auto saves session data in mongo store
-
-                res.json(sessUser); // sends cookie with sessionID automatically in response
+                console.log('sessUser', sessUser)
+                //req.session.user = sessUser; // Auto saves session data in mongo store
+                //res.json(sessUser); // sends cookie with sessionID automatically in response
+                res.render('users', { sessUser: sessUser });
             });
         });
     } else {
